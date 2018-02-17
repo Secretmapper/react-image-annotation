157c7b908710f3a10a227f88ae6fd4982ec67ed6
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2018-02-17 18:45:30 +0800

    Decompose defaultRenders from Annotation

--- src/lib/components/Annotation.js
+++ src/lib/components/Annotation.js
@@ -7,13 +7,7 @@ import withRelativeMousePos from '../utils/withRelativeMousePos'
 
 import withRectangleSelector from '../hocs/withRectangleSelector'
 import withPointSelector from '../hocs/withPointSelector'
-
-import Point from './Point'
-import Editor from './Editor'
-import FancyRectangle from './FancyRectangle'
-import Rectangle from './Rectangle'
-import Content from './Content'
-import Overlay from './Overlay'
+import defaultRenders from './defaultRenders'
 
 export default compose(
   isMouseHovering(),
@@ -78,80 +72,7 @@ export default compose(
       withRectangleSelector,
       withPointSelector
     ],
-    renderSelector: ({ annotation }) => {
-      switch (annotation.geometry.type) {
-        case withRectangleSelector.TYPE:
-          return (
-            <FancyRectangle
-              geometry={annotation.geometry}
-            />
-          )
-        case withPointSelector.TYPE:
-          return (
-            <Point
-              geometry={annotation.geometry}
-            />
-          )
-        default:
-          return null
-      }
-    },
-    renderEditor: ({ annotation, onChange, onSubmit }) => (
-      <Editor
-        data={annotation.data}
-        geometry={annotation.geometry}
-
-        onChange={onChange}
-        onSubmit={onSubmit}
-      />
-    ),
-    renderHighlight: ({ key, annotation, active }) => {
-      switch (annotation.geometry.type) {
-        case withRectangleSelector.TYPE:
-          return (
-            <Rectangle
-              key={key}
-              geometry={annotation.geometry}
-              active={active}
-            />
-          )
-        case withPointSelector.TYPE:
-          return (
-            <Point
-              key={key}
-              geometry={annotation.geometry}
-              active={active}
-            />
-          )
-        default:
-          return null
-      }
-    },
-    renderContent: ({ key, annotation }) => (
-      <Content
-        key={key}
-        geometry={annotation.geometry}
-        data={annotation.data}
-      />
-    ),
-    renderOverlay: ({ type, annotation }) => {
-      switch (type) {
-        case withRectangleSelector.TYPE:
-          return (
-            <Overlay className={styles.overlay}>
-              Click and Drag to Annotate
-            </Overlay>
-          )
-        case withPointSelector.TYPE:
-          return (
-            <Overlay className={styles.overlay}>
-              Click to Annotate
-            </Overlay>
-          )
-        default:
-          return null
-      }
-    }
+    ...defaultRenders
   }
 
   setInnerRef = (el) => {
---
+++ src/lib/components/defaultRenders.js
@@ -0,0 +1,90 @@
+import React from 'react'
+
+import styles from './Annotation.css'
+
+import Point from './Point'
+import Editor from './Editor'
+import FancyRectangle from './FancyRectangle'
+import Rectangle from './Rectangle'
+import Content from './Content'
+import Overlay from './Overlay'
+
+import withRectangleSelector from '../hocs/withRectangleSelector'
+import withPointSelector from '../hocs/withPointSelector'
+
+export default {
+  renderSelector: ({ annotation }) => {
+    switch (annotation.geometry.type) {
+      case withRectangleSelector.TYPE:
+        return (
+          <FancyRectangle
+            geometry={annotation.geometry}
+          />
+        )
+      case withPointSelector.TYPE:
+        return (
+          <Point
+            geometry={annotation.geometry}
+          />
+        )
+      default:
+        return null
+    }
+  },
+  renderEditor: ({ annotation, onChange, onSubmit }) => (
+    <Editor
+      data={annotation.data}
+      geometry={annotation.geometry}
+
+      onChange={onChange}
+      onSubmit={onSubmit}
+    />
+  ),
+  renderHighlight: ({ key, annotation, active }) => {
+    switch (annotation.geometry.type) {
+      case withRectangleSelector.TYPE:
+        return (
+          <Rectangle
+            key={key}
+            geometry={annotation.geometry}
+            active={active}
+          />
+        )
+      case withPointSelector.TYPE:
+        return (
+          <Point
+            key={key}
+            geometry={annotation.geometry}
+            active={active}
+          />
+        )
+      default:
+        return null
+    }
+  },
+  renderContent: ({ key, annotation }) => (
+    <Content
+      key={key}
+      geometry={annotation.geometry}
+      data={annotation.data}
+    />
+  ),
+  renderOverlay: ({ type, annotation }) => {
+    switch (type) {
+      case withRectangleSelector.TYPE:
+        return (
+          <Overlay className={styles.overlay}>
+            Click and Drag to Annotate
+          </Overlay>
+        )
+      case withPointSelector.TYPE:
+        return (
+          <Overlay className={styles.overlay}>
+            Click to Annotate
+          </Overlay>
+        )
+      default:
+        return null
+    }
+  }
+}
