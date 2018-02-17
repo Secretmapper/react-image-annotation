2f962c0bc1adc7bf5ff47c42dd7b72772705682a
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2018-02-17 18:21:24 +0800

    Fix FancyRectangle styles
    Revert FancyRectangle to absolute position styles

--- src/lib/components/FancyRectangle/index.js
+++ src/lib/components/FancyRectangle/index.js
@@ -10,45 +10,37 @@ function Box (props) {
 function FancyRectangle (props) {
   const { geometry } = props
 
-  const scaleX = 100 - geometry.width - geometry.x
-  const translateX = (geometry.x + geometry.width) * 100 / scaleX
-
-  const scaleY = 100 - geometry.height - geometry.y
-  const translateY = (geometry.y + geometry.height) * 100 / scaleY
-
-
   return (
-    <div className={`${props.className} ${styles.container}`} style={props.style}>
+    <div
+      className={`${props.className} ${styles.container}`}
+      style={props.style}
+    >
       <Box
         style={{
-          transform: `scaleX(100) scaleY(${geometry.y}) translate3d(0, 0, 0)`
+          height: `${geometry.y}%`,
+          width: '100%'
         }}
       />
       <Box
         style={{
-          transform: `
-            scaleX(${geometry.x})
-            scaleY(${geometry.height})
-            translate3d(0, ${geometry.y / geometry.height * 100}%, 0)
-          `
+          top: `${geometry.y}%`,
+          height: `${geometry.height}%`,
+          width: `${geometry.x}%`
         }}
       />
       <Box
         style={{
-          transform: `
-            scaleX(${scaleX})
-            scaleY(${geometry.height})
-            translate3d(${translateX}%, ${geometry.y / geometry.height * 100}%, 0)
-          `
+          top: `${geometry.y}%`,
+          left: `${geometry.x + geometry.width}%`,
+          height: `${geometry.height}%`,
+          width: `${100 - (geometry.x + geometry.width)}%`
         }}
       />
       <Box
         style={{
-          transform: `
-            scaleX(100)
-            scaleY(${scaleY})
-            translate3d(0, ${translateY}%, 0)
-          `
+          top: `${geometry.y + geometry.height}%`,
+          height: `${100 - (geometry.y + geometry.height)}%`,
+          width: '100%'
         }}
       />
     </div>
