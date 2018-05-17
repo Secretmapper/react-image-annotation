05ad314c38c084a715599b42dc10cfb58c72af84
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2018-05-17 14:31:48 +0800

    Extract TextEditor from Editor

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled, { keyframes } from 'styled-components'
+import TextEditor from '../TextEditor'
 
 const fadeInScale = keyframes`
   from {
@@ -27,39 +28,6 @@ const Container = styled.div`
   overflow: hidden;
 `
 
-const Inner = styled.div`
-  padding: 8px 16px;
-
-  textarea {
-    border: 0;
-    font-size: 14px;
-    margin: 6px 0;
-    min-height: 60px;
-    outline: 0;
-  }
-`
-
-const Button = styled.div`
-  background: whitesmoke;
-  border: 0;
-  box-sizing: border-box;
-  color: #363636;
-  cursor: pointer;
-  font-size: 1rem;
-  margin: 0;
-  outline: 0;
-  padding: 8px 16px;
-  text-align: center;
-  text-shadow: 0 1px 0 rgba(0,0,0,0.1);
-  width: 100%;
-
-  transition: background 0.21s ease-in-out;
-
-  &:focus, &:hover {
-    background: #eeeeee;
-  }
-`
-
 function Editor (props) {
   const { geometry } = props.annotation
   if (!geometry) return null
@@ -74,29 +42,17 @@ function Editor (props) {
         ...props.style
       }}
     >
-      <Inner>
-        <textarea
-          placeholder='Write description'
-          onChange={e => props.onChange({
-            ...props.annotation,
-            data: {
-              ...props.annotation.data,
-              text: e.target.value
-            }
-          })}
-          value={props.annotation.data && props.annotation.data.text}
-        >
-        </textarea>
-      </Inner>
-      {props.annotation.data
-        && props.annotation.data.text
-        && (
-        <Button
-          onClick={props.onSubmit}
-        >
-          Submit
-        </Button>
-      )}
+      <TextEditor
+        onChange={e => props.onChange({
+          ...props.annotation,
+          data: {
+            ...props.annotation.data,
+            text: e.target.value
+          }
+        })}
+        onSubmit={props.onSubmit}
+        value={props.annotation.data && props.annotation.data.text}
+      />
     </Container>
   )
 }
---
+++ src/components/TextEditor/index.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
+
+const Inner = styled.div`
+  padding: 8px 16px;
+
+  textarea {
+    border: 0;
+    font-size: 14px;
+    margin: 6px 0;
+    min-height: 60px;
+    outline: 0;
+  }
+`
+
+const Button = styled.div`
+  background: whitesmoke;
+  border: 0;
+  box-sizing: border-box;
+  color: #363636;
+  cursor: pointer;
+  font-size: 1rem;
+  margin: 0;
+  outline: 0;
+  padding: 8px 16px;
+  text-align: center;
+  text-shadow: 0 1px 0 rgba(0,0,0,0.1);
+  width: 100%;
+
+  transition: background 0.21s ease-in-out;
+
+  &:focus, &:hover {
+    background: #eeeeee;
+  }
+`
+
+function TextEditor (props) {
+  return (
+    <React.Fragment>
+      <Inner>
+        <textarea
+          placeholder='Write description'
+          onChange={props.onChange}
+          value={props.value}
+        >
+        </textarea>
+      </Inner>
+      {props.value && (
+        <Button
+          onClick={props.onSubmit}
+        >
+          Submit
+        </Button>
+      )}
+    </React.Fragment>
+  )
+}
+
+export default TextEditor
