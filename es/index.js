fef8a251090a13138cec027aedb8f29c1ee70f2e
Danilo Fuchs <danilo_fuchs@hotmail.com>
2019-10-05 22:31:25 -0300

    Remove build artifacts

--- es/index.js
+++
@@ -1,4 +0,0 @@
-import Annotation from './components/Annotation';
-export { default as defaultProps } from './components/defaultProps';
-
-export default Annotation;
--- es/selectors.js
+++
@@ -1,3 +0,0 @@
-export { default as RectangleSelector } from './hocs/RectangleSelector';
-export { default as PointSelector } from './hocs/PointSelector';
-export { default as OvalSelector } from './hocs/OvalSelector';
